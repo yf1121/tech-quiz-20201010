3c456d94658f2cb341f58ffc6a9414680f905826
yf1121 <yfujioka1121@outlook.jp>
2020-10-10 15:57:53 +0900

    get QuizAPI

--- src/screens/Quiz.js
+++ src/screens/Quiz.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+const url = "https://quizapi.io/api/v1/questions";
+
+// config for axios
+const config = {
+  params: {
+    limit: 1,
+  },
+  headers: {
+    "X-Api-Key": process.env.REACT_APP_QUIZAPI_KEY,
+  },
+};
 
 const Quiz = () => {
+  useEffect(() => {
+    fetchQuiz();
+    return;
+  }, []);
+      
+  const fetchQuiz = () => {
+    // 情報を取得
+    axios
+      .get(url, config)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        alert("エラーだよ。コンソールを見てね");
+        console.error(e);
+      })
+      .finally(() => {
+        console.log("done!");
+      });
+  }; 
   return (
     <div>
 		<h2>クイズだよ！</h2>
-		<a href="/">topに戻る</a>
+		<Link to="/">topに戻る</Link>
     </div>
   );
 };
--- src/screens/Top.js
+++ src/screens/Top.js
@@ -2,12 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Top = () => {
-  return (
-    <div>
-		<h2>topだよ！</h2>
-		<Link to="/quiz">quizをやる</Link>
-    </div>
-  );
+    return (
+        <div className="top-container center-align">
+            <h4 className="top-header">
+            クイズを解いて
+            <br />
+            圧倒的成長！
+            </h4>
+            <div>
+            <Link to="/quiz" className="btn-large green">
+                クイズをやる
+            </Link>
+            </div>
+        </div>
+    );    
 };
 
 export default Top;
